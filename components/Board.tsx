406a6286bb599f9e8a694512ae2cabf0f2201389
Isti <istvan.pasztor@partners.neste.com>
2023-06-13 19:02:41 +0300

    create board components dnd boilerplate

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,13 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
 export const Board = () => {
   return (
-    <div>Board</div>
-  )
-}
+    <div>
+      <DragDropContext>
+        <Droppable droppableId="board" direction="horizontal" type="column">
+          {(provided) => <div>{/* rendering all the columns */}</div>}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
