f7634c06c801518bbe92ca109cd215d9edac1416
Isti <istvan.pasztor@partners.neste.com>
2023-06-13 20:53:06 +0300

    update Board component to map through fetched data

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,18 +1,40 @@
 "use client";
 
+import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 export const Board = () => {
+  // TODO: check if it works with object destructuring
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  const handleOnDragEnd = (result: DropResult) => {};
+
   useEffect(() => {
-    // getBoard()
-  }, []);
+    getBoard();
+  }, [getBoard]);
 
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="board" direction="horizontal" type="column">
-          {(provided) => <div>{/* rendering all the columns */}</div>}
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            >
+              {Array.from(board.columns.entries()).map(
+                ([id, column], index) => (
+                  <Column key={id} id={id} todos={column.todos} index={index} />
+                )
+              )}
+            </div>
+          )}
         </Droppable>
       </DragDropContext>
     </div>
