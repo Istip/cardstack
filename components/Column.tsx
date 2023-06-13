a921e28bef9d0bd0f626663b6ac1f846fee45d47
Isti <istvan.pasztor@partners.neste.com>
2023-06-13 21:37:55 +0300

    create different columns for every todo status

---
+++ components/Column.tsx
@@ -0,0 +1,79 @@
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import TodoCard from "./TodoCard";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+
+type Props = {
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+
+const idToColumnText: { [key in TypedColumn]: string } = {
+  todo: "TO DO",
+  inprogress: "IN PROGRESS",
+  done: "DONE",
+};
+
+function Column({ id, todos, index }: Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {/* render droppable for todos */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                }`}
+              >
+                <h2 className="flex justify-between items-center font-bold text-xl">
+                  {idToColumnText[id]}
+                  <span className="flex items-center text-white bg-sky-400 rounded-full py-1 px-2 text-sm">
+                    {todos.length}
+                  </span>
+                </h2>
+
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {provided.placeholder}
+
+                  <div className="flex items-end justify-end">
+                    <button className="text-green-500 hover:text-green-600 transition-all">
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  );
+}
+export default Column;
