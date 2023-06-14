1b983daaafcd79dda55a79311acf33763373baab
Istip <isticsek@gmail.com>
2023-06-14 20:05:23 +0300

    create todo card boilerplate

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,44 @@
-function TodoCard() {
-  return <div>TodoCard</div>;
+"use client";
+
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+function TodoCard({
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex items-center justify-between p-5 font-light text-sm">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600 transition-all">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+
+      {/* Image here if exists */}
+    </div>
+  );
 }
 export default TodoCard;
