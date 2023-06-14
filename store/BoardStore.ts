bb7aa9500929a26855702b391a6a85ffdba01835
Istip <isticsek@gmail.com>
2023-06-14 20:05:10 +0300

    update board store with update functionality

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,17 +1,35 @@
+import { databases } from "@/appwrite";
 import { getTodosGrouppedByColumn } from "@/utils/getTodosGrouppedByColumn";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodo: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<TypedColumn, Column>(),
   },
+
   getBoard: async () => {
     const board = await getTodosGrouppedByColumn();
     set({ board });
+  },
+
+  setBoardState: (board) => set({ board }),
+
+  updateTodo: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_TODOS_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   },
 }));
