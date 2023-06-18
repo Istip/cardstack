98aa51993a01235c5c6a0e79e5c5e75a496b2459
Istip <isticsek@gmail.com>
2023-06-18 19:54:01 +0300

    add modal to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#f5f6f8]`}>{children}</body>
+      <body className={`${inter.className} bg-[#f5f6f8]`}>
+        <Modal />
+        {children}
+      </body>
     </html>
   );
 }
